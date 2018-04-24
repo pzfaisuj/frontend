93aa2896b45a0bbd8e8d63171c02b463ba1cd3f6
Bartosz Bukowiec <bartosz.bukowiec@synerise.com>
2018-04-24 17:34:05 +0200

    new branch init

--- js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#products-table').DataTable({
-        language: {
-            "url": "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Polish.json"
-        }
-    });
-});
