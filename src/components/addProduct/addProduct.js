f2193976f70931b0edf7d862d2d89da311965cd3
Bartosz Bukowiec <bartosz.bukowiec@synerise.com>
2018-06-24 19:20:58 +0200

    add fix

--- src/components/addProduct/addProduct.js
+++ src/components/addProduct/addProduct.js
@@ -12,28 +12,30 @@ addProduct = (e) => {
     const productCode = e.target.elements.productCode.value;
     const formDescription = e.target.elements.formDescription.value;
     const photoUrl = e.target.elements.photoUrl.value;
-    console.log('sell price: ' + productSellPrice);
-    console.log('product name: ' + productName);
-    console.log('product code: ' + productCode);
-    console.log('description: ' + formDescription);
-    console.log('photoUrl: ' + photoUrl);
-
-    axios.post('http://localhost:8095/products', {
-        name: productName ,
-        price: productSellPrice ,
-        productCode: productCode ,
-        description: formDescription ,
-        imageURL: photoUrl
-      })
-      .then(function (response) {
-        console.log('Produkt został dodany');
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log('Błąd podczas dodania produktu');
-        console.log(error);
-      });
-
+      const dt = JSON.stringify(
+        {
+            name: productName,
+            price: productSellPrice,
+            productCode: productCode,
+            description: formDescription,
+            imageURL: photoUrl
+        }
+    );
+      console.log('product: ' + dt);
+      axios({
+          method: 'post',
+          url: 'http://localhost:8095/products',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          data: dt
+        }).then((res) => {
+            alert('dodano produkt');
+            console.log(res);
+            window.location.reload();
+        }).catch((err) => {
+          alert('AXIOS addProduct FAILED', err);
+        });
 }
 
     render() {
